091c0f29d5c231576e17e96c1eb601d0ddedf11d
NIASSE Fama <fama.niasse@renault.com>
2024-05-28 10:48:48 +0200

    blockeurs backend

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="bg-white">
+            <div>
+                <img src="" alt="" />
+            </div>
+
+        </header>
+        
+)
+  }
